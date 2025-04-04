96042b73a1a02b3c3ae7e19c21d866d4dc2a32d4
Tomas Ljutenko <zloutek1@gmail.com>
2025-04-04 19:47:04 +0200

    added enableRedistributableFirmware

--- modules/common.nix
+++ modules/common.nix
@@ -10,6 +10,7 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   nixpkgs.config.allowUnfree = true;
+  hardware.enableRedistributableFirmware = true;
 
   environment.systemPackages = with pkgs; [
     # Applications
