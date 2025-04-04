b6a72ca59bc198c5c49980769d1581823fc115da
Tomas Ljutenko <zloutek1@gmail.com>
2025-04-04 21:18:44 +0200

    fix typo

--- pkgs/elegant-grub-theme.nix
+++ pkgs/elegant-grub-theme.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
     pname = "elegant-grub-theme";
     version = "0.1";
 
-    src = pkgs.fetchFromGithub {
+    src = pkgs.fetchFromGitHub {
         owner = "vinceliuice";
         repo = "Elegant-grub2-themes";
         rev = "7caa304b349ee638481935d5e0d82b33033b0b1c";
