c10fa052314fd48a1bb544c45427eab8bacbaffe
Tomas Ljutenko <zloutek1@gmail.com>
2025-04-04 21:19:14 +0200

    updated sha

--- pkgs/elegant-grub-theme.nix
+++ pkgs/elegant-grub-theme.nix
@@ -8,7 +8,7 @@ pkgs.stdenv.mkDerivation {
         owner = "vinceliuice";
         repo = "Elegant-grub2-themes";
         rev = "7caa304b349ee638481935d5e0d82b33033b0b1c";
-        sha256 = "";
+        sha256 = "sha256-mBhG3fFYUIE2Sf2h4PVLhbC9PboZaAx1EpaQ7NX9NtE=";
     };
 
 }
