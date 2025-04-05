4632bbe96956f3717de7fb919b5645f8c4f86360
Tomas Ljutenko <zloutek1@gmail.com>
2025-04-05 14:21:15 +0200

    make grub remember selected option

--- modules/grub.nix
+++ modules/grub.nix
@@ -17,6 +17,7 @@
                 enable = true;
                 efiSupport = true;
                 device = "nodev";
+                default = "saved";
                 
                 useOSProber = true;
                 
