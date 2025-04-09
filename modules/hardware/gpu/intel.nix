236b88ab85af7ced129b535fe9c346549e7c7f58
Tomas Ljutenko <zloutek1@gmail.com>
2025-04-09 13:24:15 +0200

    refactor: removed unused files

--- modules/hardware/gpu/intel.nix
+++
@@ -1,9 +0,0 @@
-{lib, ...}: {
-  
-  config = {
-    boot.kernelModules = ["kvm-intel"];
-    hardware.cpu.intel.updateMicrocode = true;
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  };
-  
-}
