8311ff0b66c2077e9cfe2bf868f1b5cf3d2a0bba
Tomas Ljutenko <zloutek1@gmail.com>
2025-04-15 23:08:57 +0200

    removed hyprland shade

--- modules/home-manager/hyprland/style.nix
+++ modules/home-manager/hyprland/style.nix
@@ -6,10 +6,6 @@
       border_size = 2;
       gaps_in = 5;
       gaps_out = 20;
-      
-      #resize_on_border = false;
-      #allow_tearing = false;
-      #layout = "dwindle";
 
       "col.active_border" = "$primary $primary_container 45deg";
       "col.inactive_border" = "$on_surface";
@@ -21,15 +17,6 @@
       active_opacity = 1.0;
       inactive_opacity = 1.0;
       fullscreen_opacity = 1.0;
-
-      shadow = {
-        enabled = true;
-        range = 30;
-        render_power = 10;
-
-        color = "$shadow";
-        color_inactive = "$shadow";
-      };
 
       blur = {
         enabled = true;
--- modules/home-manager/hyprland/workspaces.nix
+++ modules/home-manager/hyprland/workspaces.nix
@@ -9,65 +9,65 @@ in
     bind = [
       #---------------move focus with mainMod + arrow keys (layout agnostic)------------------------------#
       #---------------------------------------------------------------------------------------------------#
-      "${mainMod}, left, movefocus, l"
-      "${mainMod}, right, movefocus, r"
-      "${mainMod}, up, movefocus, u"
-      "${mainMod}, down, movefocus, d"
+      "$mainMod, left, movefocus, l"
+      "$mainMod, right, movefocus, r"
+      "$mainMod, up, movefocus, u"
+      "$mainMod, down, movefocus, d"
       
       #---------------move/resize windows with mainMod + arrow keys-------------------------------#
       #-------------------------------------------------------------------------------------------#
-      "${mainMod} SHIFT, left, movewindow, l"
-      "${mainMod} SHIFT, right, movewindow, r"
-      "${mainMod} SHIFT, up, movewindow, u"
-      "${mainMod} SHIFT, down, movewindow, d"
+      "$mainMod SHIFT, left, movewindow, l"
+      "$mainMod SHIFT, right, movewindow, r"
+      "$mainMod SHIFT, up, movewindow, u"
+      "$mainMod SHIFT, down, movewindow, d"
       
       #---------------switch workspaces with mainMod + [0-9]----------------------------#
       #---------------------------------------------------------------------------------#
-      "${mainMod}, 1, workspace, 1"
-      "${mainMod}, 2, workspace, 2"
-      "${mainMod}, 3, workspace, 3"
-      "${mainMod}, 4, workspace, 4"
-      "${mainMod}, 5, workspace, 5"
-      "${mainMod}, 6, workspace, 6"
-      "${mainMod}, 7, workspace, 7"
-      "${mainMod}, 8, workspace, 8"
-      "${mainMod}, 9, workspace, 9"
-      "${mainMod}, 0, workspace, 10"
+      "$mainMod, 1, workspace, 1"
+      "$mainMod, 2, workspace, 2"
+      "$mainMod, 3, workspace, 3"
+      "$mainMod, 4, workspace, 4"
+      "$mainMod, 5, workspace, 5"
+      "$mainMod, 6, workspace, 6"
+      "$mainMod, 7, workspace, 7"
+      "$mainMod, 8, workspace, 8"
+      "$mainMod, 9, workspace, 9"
+      "$mainMod, 0, workspace, 10"
       
       #---------------move active window to a workspace with mainMod + SHIFT + [0-9]-------------------#
       #------------------------------------------------------------------------------------------------#
-      "${mainMod} SHIFT, 1, movetoworkspacesilent, 1"
-      "${mainMod} SHIFT, 2, movetoworkspacesilent, 2"
-      "${mainMod} SHIFT, 3, movetoworkspacesilent, 3"
-      "${mainMod} SHIFT, 4, movetoworkspacesilent, 4"
-      "${mainMod} SHIFT, 5, movetoworkspacesilent, 5"
-      "${mainMod} SHIFT, 6, movetoworkspacesilent, 6"
-      "${mainMod} SHIFT, 7, movetoworkspacesilent, 7"
-      "${mainMod} SHIFT, 8, movetoworkspacesilent, 8"
-      "${mainMod} SHIFT, 9, movetoworkspacesilent, 9"
-      "${mainMod} SHIFT, 0, movetoworkspacesilent, 10"
+      "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
+      "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
+      "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
+      "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
+      "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
+      "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
+      "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
+      "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
+      "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
+      "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
       
-      "CTRL ${mainMod}, right, movecurrentworkspacetomonitor, r"
-      "CTRL ${mainMod}, left, movecurrentworkspacetomonitor, l"
-      "CTRL ${mainMod}, up, movecurrentworkspacetomonitor, u"
-      "CTRL ${mainMod}, down, movecurrentworkspacetomonitor, d"
+      "CTRL $mainMod, right, movecurrentworkspacetomonitor, r"
+      "CTRL $mainMod, left, movecurrentworkspacetomonitor, l"
+      "CTRL $mainMod, up, movecurrentworkspacetomonitor, u"
+      "CTRL $mainMod, down, movecurrentworkspacetomonitor, d"
       
       #------------------------special workspace (scratchpad)---------------------------#
       #---------------------------------------------------------------------------------#
-      "${mainMod}, S, togglespecialworkspace, magic"
-      "${mainMod} SHIFT, S, movetoworkspace, special:magic"
+      "$mainMod, S, togglespecialworkspace, magic"
+      "$mainMod SHIFT, S, movetoworkspace, special:magic"
       
       #---------------scroll through existing workspaces with mainMod + scroll-------------------#
       #------------------------------------------------------------------------------------------#
-      "${mainMod}, mouse_down, workspace, e+1"
-      "${mainMod}, mouse_up, workspace, e-1"
+      "$mainMod, mouse_down, workspace, e+1"
+      "$mainMod, mouse_up, workspace, e-1"
     ];
 
     bindm = [
       #---------------move/resize windows with mainMod + LMB/RMB and dragging-------------------#
       #-------------------------------------------------------------------------------------------#
-      "${mainMod}, mouse:272, movewindow"
-      "${mainMod}, mouse:273, resizewindow"
+      "$mainMod, mouse:272, movewindow"
+      "$mainMod, mouse:273, resizewindow"
     ];
   };
 }
