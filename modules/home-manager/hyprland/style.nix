991d2b72b85db6b39bff9d95bba94be1a178b482
Tomas Ljutenko <zloutek1@gmail.com>
2025-04-16 00:54:06 +0200

    renamed hyprland animation

--- modules/home-manager/hyprland/style.nix
+++ modules/home-manager/hyprland/style.nix
@@ -31,9 +31,19 @@
 
     animations = {
       enabled = true;
-      bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+      
+      bezier = [
+        "wind, 0.05, 0.9, 0.1, 1.05"
+        "winIn, 0.1, 1.1, 0.1, 1.1"
+        "winOut, 0.3, -0.3, 0, 1"
+        "liner, 1, 1, 1, 1"
+        "overshot, 0.05, 0.9, 0.1, 1.05"
+        "smoothOut, 0.5, 0, 0.99, 0.99"
+        "smoothIn, 0.5, -0.5, 0.68, 1.5"
+      ];
+      
       animation = [
-        "windows, 1, 7, myBezier"
+        "windows, 1, 7, wind"
         "windowsOut, 1, 7, default, popin 80%"
         "border, 1, 10, default"
         "borderangle, 1, 8, default"
