6fa0cbd2c27b32e11ce49d51a0727d98a3c5aabe
Tomas Ljutenko <zloutek1@gmail.com>
2025-04-15 22:21:30 +0200

    fixed monitor scaling

--- modules/home-manager/hyprland/monitor.nix
+++ modules/home-manager/hyprland/monitor.nix
@@ -3,9 +3,20 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "HDMI-A-1, 1920x1080, 0x-1200, 1"
-      "eDP-1,    1920x1200, 0x0,    1"
-      "Unknown-1, disable"
+      #"HDMI-A-1, 1920x1080, 0x-1200, 1"
+      #"eDP-1,    3200x2000, 0x0,     3"
+      #"Unknown-1, disable"
+      ", highres, auto, 2"
+    ];
+
+    xwayland = {
+      force_zero_scaling = true;
+    };
+
+    # toolkit-specific scale
+    env = [
+      "GDK_SCALE,2"
+      "XCURSOR_SIZE,32"
     ];
   };
 }
