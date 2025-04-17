edb51aabe317f01f9a0b52be249aa75fb735ebb2
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-17 23:49:23 +0200

    fixed wofi color init

--- modules/home-manager/wofi/default.nix
+++ modules/home-manager/wofi/default.nix
@@ -23,9 +23,9 @@
     style = builtins.readFile ./style.css;
   };
 
-  home.activation.writeWaybarTheme = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if [ ! -f $HOME/.config/waybar/colors.css ]; then
-      cat "${./colors.css}" > "$HOME/.config/waybar/colors.css"
+  home.activation.writeWofiTheme = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -f $HOME/.config/wofi/colors.css ]; then
+      cat "${./colors.css}" > "$HOME/.config/wofi/colors.css"
     fi
   '';
 
