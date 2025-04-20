4972ac862a868ecda69d16fe3f26692a30d57fe5
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-20 22:53:43 +0200

    enabled blueman

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -4,9 +4,23 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+      General = {
+        Name = "Hello";
+        ControllerMode = "dual";
+        FastConnectable = "true";
+        Experimental = "true";
+      };
+      Policy = {
+        AutoEnable = "true";
+      };
+    };
   };
 
-  # services.blueman.enable = true;
+  services.blueman = {
+    enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     overskride
   ];
