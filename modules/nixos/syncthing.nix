63604c91fa4c4e0faa04b23707017bf3fe52d53f
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-21 00:06:27 +0200

    removed old settings

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -3,12 +3,14 @@
   # TODO: move to a home manager module
   services.syncthing = {
     enable = true;
-    dataDir = "/home/${username}";
     openDefaultPorts = true;
-    configDir = "/home/${username}/.config/syncthing";
-    user = username;
-    group = "users";
-    guiAddress = "0.0.0.0:8384";
+
+    settings = {
+      gui = {
+        user = username;
+        password = "pass123";
+      };
+    };
   };
 
 }
