923ff749f3e8b273ec4f8fe893d31685d59863a0
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-20 23:52:00 +0200

    replaced user with variable

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -6,7 +6,7 @@
     dataDir = "/home/${username}";
     openDefaultPorts = true;
     configDir = "/home/${username}/.config/syncthing";
-    user = "tomas";
+    user = username;
     group = "users";
     guiAddress = "0.0.0.0:8384";
   };
