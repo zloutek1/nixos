6e0b4bd8d1d84a7191ac9eae02ed954c1d70f244
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-20 02:29:22 +0200

    fix quotes

--- modules/home-manager/hyprland/hyprshade.nix
+++ modules/home-manager/hyprland/hyprshade.nix
@@ -7,8 +7,8 @@
   xdg.configFile."hypr/hyprshade.toml".text = ''
     [[shades]]
     name = "blue-light-filter"
-    start_time = "19:00:00"
-    end_time = "06:00:00"
+    start_time = 19:00:00
+    end_time = 06:00:00
   '';
 
 }
