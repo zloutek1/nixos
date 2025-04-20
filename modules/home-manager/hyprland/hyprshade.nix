2a61c698d2fa93b5e65d45f9f83cd8ef38d0dd64
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-20 02:28:51 +0200

    fixed hyprshade

--- modules/home-manager/hyprland/hyprshade.nix
+++ modules/home-manager/hyprland/hyprshade.nix
@@ -4,12 +4,11 @@
     hyprshade
   ];
 
-  xdg.configFile."hypr/hyprshade.toml".text = inputs.nix-std.lib.serde.toTOML {
-    shades = {
-      name = "blue-light-filter";
-      start_time = "19:00:00";
-      end_time = "06:00:00";
-    };
-  };
+  xdg.configFile."hypr/hyprshade.toml".text = ''
+    [[shades]]
+    name = "blue-light-filter"
+    start_time = "19:00:00"
+    end_time = "06:00:00"
+  '';
 
 }
