da43e4bc07484d67d4dd75246dc369ff730e4010
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-23 18:15:36 +0200

    fixed wallchange not running automatically

--- modules/home-manager/waybar/modules/wallchange.nix
+++ modules/home-manager/waybar/modules/wallchange.nix
@@ -5,12 +5,15 @@
   programs.waybar.settings.${barName} = {
     "custom/wallchange" = {
       format = "󰆊{}";
-      exec = "echo ; echo 󰆊 switch wallpaper";
+      
+      exec = "wallchange next";
+      interval = 900; # once 15min
+
       on-click = "wallchange next";
       on-click-right = "sleep 0.1 && pick-wallpaper";
-      #on-click-right = "next-wallpaper --previous";
-      interval = 900; # once 15min
+      
       tooltip = true;
+      tooltip-format = "󰆊 switch wallpaper";
     };
   };
   
