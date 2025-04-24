1016cf5037453d776ab84d58069e555c011670bf
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-24 19:57:58 +0200

    added hashed password

--- users/tomas/nixos.nix
+++ users/tomas/nixos.nix
@@ -5,6 +5,7 @@
     description = username;
     extraGroups = [ "wheel" "networkmanager" "video" ];
     shell = pkgs.zsh;
+    hashedPassword = "$6$PdrZNYAaiJjGTBbJ$Gp2lZ6Vkh5GsYawp0nGtPxCeVu/wJLDjT2dH3O0kfI5mjpmrMtOmk.RgTcNjp/RHP3YsQDoz1TWUbYfZDLdMv1";
   };
   
   environment.shells = with pkgs; [
