38b485a042fbc8236fd9ea9510514e24c64d690c
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-25 18:36:42 +0200

    update logout modules

--- modules/home-manager/wlogout.nix
+++ modules/home-manager/wlogout.nix
@@ -1,7 +1,45 @@
 { ... }: {
 
-  programs.wlogout = {
+   programs.wlogout = {
     enable = true;
+    layout = [
+      {
+        "label"  = "lock";
+        "action" = "hyprlock";
+        "text" = "Lock";
+        "keybind" = "l";
+      }
+      {
+        "label" = "shutdown";
+        "action" = "systemctl poweroff";
+        "text" = "Shutdown";
+        "keybind" = "s";
+      }
+      {
+        "label" = "suspend";
+        "action" = "systemctl suspend";
+        "text" = "Suspend";
+        "keybind" = "u";
+      }
+      {
+        "label" = "reboot";
+        "action" = "systemctl reboot";
+        "text" = "Reboot";
+        "keybind" = "r";
+      }
+      {
+        "label" = "hibernate";
+        "action" = "systemctl hibernate";
+        "text" = "Hibernate";
+        "keybind" = "h";
+      }
+      {
+        "label" = "reboot";
+        "action" = "systemctl reboot";
+        "text" = "Reboot";
+        "keybind" = "r";
+      }
+    ];
   };
 
 }
