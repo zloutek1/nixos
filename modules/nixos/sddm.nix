d740c276d35c402b2c72e33521e9430ad69d641a
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-26 02:23:02 +0200

    enable numlock on sddm

--- modules/nixos/sddm.nix
+++ modules/nixos/sddm.nix
@@ -12,4 +12,15 @@
 
   environment.systemPackages = [ pkgs.sddm-astronaut ];
 
+  systemd.services.numLockOnTty = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = lib.mkForce (pkgs.writeShellScript "numLockOnTty" ''
+        for tty in /dev/tty{1..6}; do
+            ${pkgs.kbd}/bin/setleds -D +num < "$tty";
+        done
+      '');
+    };
+  };
+
 }
