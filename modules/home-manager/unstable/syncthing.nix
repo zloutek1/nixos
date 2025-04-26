1bef8b22b68fbf23cfbbf4073e177f5a757eaf35
Tomáš Ljutenko <tomas.ljutenko@gmail.com>
2025-04-26 18:05:10 +0200

    defined syncthing devices and folders

--- modules/home-manager/unstable/syncthing.nix
+++ modules/home-manager/unstable/syncthing.nix
@@ -13,7 +13,28 @@
     overrideFolders = true;
 
     settings = {
+      devices = {
+        "mac-mini" = {
+          id = "NHUD4JQ-L5TBR4T-HJGKWMF-2AICEWV-IE5U5I5-2VWSFEJ-7ROVVFH-55YTKQ7";
+        }; 
+      };
 
+      folders = {
+        "/home/tomas/documents/personal" = {
+          id = "life-archive";
+          devices = [ "mac-mini" ];
+        };
+
+        "/home/tomas/documents/KeePassXC" = {
+          id = "keepass";
+          devices = [ "mac-mini" ];
+        };
+
+        "/home/tomas/projects" = {
+          id = "projects";
+          devices = [ "mac-mini" ];
+        };
+      };
     };
   };
 
