e735fadfaea987fade9853bb4f1589fc04c75738
Tomáš Ljutenko <zloutek1@gmail.com>
2025-05-03 20:40:41 +0200

    added common programs for darwin

--- modules/darwin/common.nix
+++ modules/darwin/common.nix
@@ -7,4 +7,24 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  environment.systemPackages = with pkgs; [
+    # Applications
+    
+    # Development
+    git
+    
+    # Utilities
+    neovim
+    tree
+    zip
+    unzip
+    wget
+    curl
+    htop
+    jq                  # A lightweight and flexible command-line JSON processor
+    fastfetch           # neofetch alternative
+    bat                 # better cat
+    nixfmt-rfc-style    # Nix formatter
+  ];
+
 }
--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -1,37 +1,36 @@
 { self, pkgs, ... }:
 
 {
-    imports = [
-        ./nix.nix
-        ./locale.nix
-        ./fonts.nix
-    ];
+  imports = [
+    ./nix.nix
+    ./locale.nix
+    ./fonts.nix
+  ];
 
-    nixpkgs.config.allowUnfree = true;
-    hardware.enableRedistributableFirmware = true;
+  nixpkgs.config.allowUnfree = true;
+  hardware.enableRedistributableFirmware = true;
 
-    environment.systemPackages = with pkgs; [
-        # Applications
-        nautilus            # file manager
+  environment.systemPackages = with pkgs; [
+    # Applications
+    nautilus            # file manager
 
-        # Development
-        git
-        
-        # Utilities
-        neovim
-        tree
-        zip
-        unzip
-        wget
-        curl
-        htop
-        jq                  # A lightweight and flexible command-line JSON processor
-        fastfetch           # neofetch alternative
-        nsxiv               # image viewer
-        stow                # dotfiles manager
-        bat                 # better cat
-        nixfmt-rfc-style    # Nix formatter
-    ];
+    # Development
+    git
+    
+    # Utilities
+    neovim
+    tree
+    zip
+    unzip
+    wget
+    curl
+    htop
+    jq                  # A lightweight and flexible command-line JSON processor
+    fastfetch           # neofetch alternative
+    nsxiv               # image viewer
+    bat                 # better cat
+    nixfmt-rfc-style    # Nix formatter
+  ];
 
-    hardware.graphics.enable = true;
+  hardware.graphics.enable = true;
 }
